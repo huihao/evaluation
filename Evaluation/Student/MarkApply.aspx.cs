d7f371758ac79b158da6d689a68b1ece2f016671
cs <770596367@qq.com>
2014-05-07 16:20:53 +0800

    markapply

---
+++ Evaluation/Student/MarkApply.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eva.Evaluation.Student
+{
+    public partial class MarkApply : System.Web.UI.Page
+    {
+        Eva.BLL.Mark bllMark = new BLL.Mark();
+        Eva.Model.Mark mark = new Model.Mark();
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                int id = int.Parse(Request["id"]);
+                mark = bllMark.GetModel(id);
+                
+                txtCourseName.Text = Eva.BLL.Utils.GetCourseName(Convert.ToInt16(mark.CourseId));
+                txtScore.Text = mark.Score.ToString();
+
+            }
+        }
+
+        protected void Save_Click(object sender, EventArgs e)
+        {
+            string strErr = "";
+            if (txtBonusPoint.Text.Trim().Length == 0)
+            {
+                strErr += "加分的分数不能为空！";
+            }
+            if (strErr != "")
+            {
+                Maticsoft.Common.MessageBox.Show( this,strErr);
+                return;
+            }
+            mark.BonusPoint =  Convert.ToDecimal( txtBonusPoint.Text);
+            mark.CheckStep = 1;
+            bllMark.Update(mark);
+            Maticsoft.Common.MessageBox.ShowAndRedirect(this, "保存成功！", "ShowMark.aspx");
+        }
+    }
+}
---
+++ Evaluation/Student/ShowMark.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eva.Evaluation.Student
+{
+    public partial class ShowMark : System.Web.UI.Page
+    {
+        BLL.Mark markBll = BLL.BLLFactory.GetMarkBLLInstance();
+        BLL.WebUser userBll = BLL.BLLFactory.GetWebUserBLLInstance();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (!IsPostBack)
+                {
+
+                    MarkBind();
+                }
+            }
+        }
+
+
+        protected void MarkBind()
+        {
+            //int id = int.Parse(Request["id"]);
+            var set = markBll.GetListByStudentId(1);
+            MarkRepeater.DataSource = set;
+            MarkRepeater.DataBind();
+        }
+
+        protected void YearList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MarkBind();
+        }
+
+        protected void Termlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MarkBind();
+        }
+
+        protected void MarkRepeater_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "apply")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+                Response.Redirect("MarkApply.aspx?id="+id);
+            }
+
+        }
+    }
+}
